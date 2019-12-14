4f0149d82d879764befeb10bf9a8305c3f5cb2a0
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 09:33:30 -0500

    added #total

--- 01/fuel_requirement.rb
+++ 01/fuel_requirement.rb
@@ -2,4 +2,8 @@ class FuelRequirement
   def for_mass(mass)
     mass / 3 - 2
   end
+
+  def total(ary_of_req)
+    ary_of_req.sum
+  end
 end
--- 01/fuel_requirement_test.rb
+++ 01/fuel_requirement_test.rb
@@ -11,4 +11,10 @@ class FuelRequirementTest < Minitest::Test
     assert_equal fuel_required.for_mass(1969), 654
     assert_equal fuel_required.for_mass(100756), 33583
   end
+
+  def test_calculates_total_fuel_needed
+    fuel_required = FuelRequirement.new
+
+    assert_equal fuel_required.total([12, 14, 1969, 100756]), 102751
+  end
 end
