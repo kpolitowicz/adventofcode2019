fa5a9e39fa3e3cf41652e47610054c4c7d4416e4
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 14:33:37 -0500

    figured out how to construct list of inputs to check

--- 02/intcode_input_finder.exs
+++ 02/intcode_input_finder.exs
@@ -1,4 +1,8 @@
 defmodule IntcodeInputFinder do
+  def find_inputs_for(output, program, arg1_range, arg2_range) do
+    inputs_to_check = List.zip([Enum.to_list(arg1_range), Enum.to_list(arg2_range)])
+  end
+
   def encode_input(noun, verb) do
     100 * noun + verb
   end
--- 02/intcode_input_finder_test.exs
+++ 02/intcode_input_finder_test.exs
@@ -5,6 +5,10 @@ Code.require_file "intcode_input_finder.exs", __DIR__
 defmodule IntcodeInputFinderTest do
   use ExUnit.Case
 
+  test "finds the program inputs that give desired output" do
+    assert {0,0} = IntcodeInputFinder.find_inputs_for(1, {1,0,0,0,99}, 0..1, 2..3)
+  end
+
   test "encodes noun and verb" do
     assert 1202 = IntcodeInputFinder.encode_input(12, 2)
   end
