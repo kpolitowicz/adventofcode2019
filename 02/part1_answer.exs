3dc86abfca479a12cc4e662d76ca905aad9caca2
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 13:53:07 -0500

    added noun+verb terminology

--- 02/part1_answer.exs
+++ 02/part1_answer.exs
@@ -2,10 +2,12 @@ Code.require_file "intcode_program.exs", __DIR__
 
 {:ok, program_str} = File.read(__DIR__ <> "/input.txt")
 
+noun = 12
+verb = 2
 program =
   IntcodeProgram.read_from(program_str)
-  |> put_elem(1, 12)
-  |> put_elem(2, 2)
+  |> put_elem(1, noun)
+  |> put_elem(2, verb)
   |> IntcodeProgram.run
 
 IO.puts elem(program, 0)
