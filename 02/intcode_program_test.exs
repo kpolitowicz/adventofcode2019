9780d3ea4e8a927308e983976697247d4e91af20
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 12:00:58 -0500

    added first "real" test for the problem

--- 02/intcode_program_test.exs
+++ 02/intcode_program_test.exs
@@ -5,6 +5,11 @@ Code.require_file "intcode_program.exs", __DIR__
 defmodule IntcodeProgramTest do
   use ExUnit.Case
 
+  test "converts comma-separated string into a tuple" do
+    string = "1,2,3"
+    assert {1,2,3} = IntcodeProgram.read_from(string)
+  end
+
   test "printable?" do
     assert IntcodeProgram.printable?('abc') == true
   end
