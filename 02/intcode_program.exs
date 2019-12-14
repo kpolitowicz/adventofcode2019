3c8a80e641e65d2289992b38317a2ef57d17cf21
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 13:48:26 -0500

    changed opcode -> instuction

--- 02/intcode_program.exs
+++ 02/intcode_program.exs
@@ -7,34 +7,34 @@ defmodule IntcodeProgram do
   end
 
   def run(program) do
-    execute_next_opcode(program, 0)
+    execute_next_instruction(program, 0)
   end
 
-  defp execute_next_opcode(program, current_position) do
-    opcode = elem(program, current_position)
+  defp execute_next_instruction(program, current_position) do
+    instruction = elem(program, current_position)
 
-    case execute_opcode(opcode, program, current_position) do
+    case execute_instruction(instruction, program, current_position) do
       {:ok, result} ->
         res_pos = elem(program, current_position + 3)
         program = put_elem(program, res_pos, result)
-        execute_next_opcode(program, current_position + 4)
+        execute_next_instruction(program, current_position + 4)
 
       :halt ->
         program
     end
   end
 
-  defp execute_opcode(1, program, current_position) do
+  defp execute_instruction(1, program, current_position) do
     arg1_pos = elem(program, current_position + 1)
     arg2_pos = elem(program, current_position + 2)
 
     {:ok, elem(program, arg1_pos) + elem(program, arg2_pos)}
   end
-  defp execute_opcode(2, program, current_position) do
+  defp execute_instruction(2, program, current_position) do
     arg1_pos = elem(program, current_position + 1)
     arg2_pos = elem(program, current_position + 2)
 
     {:ok, elem(program, arg1_pos) * elem(program, arg2_pos)}
   end
-  defp execute_opcode(99, _program, _current_position), do: :halt
+  defp execute_instruction(99, _program, _current_position), do: :halt
 end
