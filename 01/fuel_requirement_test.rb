167f3d6d11fe9d3e0f0fe2b3b68bdb19a91d4441
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 09:28:51 -0500

    changed the var in tests

--- 01/fuel_requirement_test.rb
+++ 01/fuel_requirement_test.rb
@@ -4,11 +4,11 @@ require_relative 'fuel_requirement'
 
 class FuelRequirementTest < Minitest::Test
   def test_calculates_fuel_requirements
-    answer = FuelRequirement.new
+    fuel_required = FuelRequirement.new
 
-    assert_equal answer.fuel_required_for_mass_of(12), 2
-    assert_equal answer.fuel_required_for_mass_of(14), 2
-    assert_equal answer.fuel_required_for_mass_of(1969), 654
-    assert_equal answer.fuel_required_for_mass_of(100756), 33583
+    assert_equal fuel_required.fuel_required_for_mass_of(12), 2
+    assert_equal fuel_required.fuel_required_for_mass_of(14), 2
+    assert_equal fuel_required.fuel_required_for_mass_of(1969), 654
+    assert_equal fuel_required.fuel_required_for_mass_of(100756), 33583
   end
 end
