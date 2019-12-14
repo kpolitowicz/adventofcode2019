005b38ee9a53871af873ce5444112e23f15b0ffe
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 12:03:31 -0500

    inverted assert_equal params in 01 - teh expected answer comes first

--- 01/fuel_requirement_test.rb
+++ 01/fuel_requirement_test.rb
@@ -6,23 +6,23 @@ class FuelRequirementTest < Minitest::Test
   def test_calculates_fuel_requirements_for_given_mass
     fuel_required = FuelRequirement.new
 
-    assert_equal fuel_required.for_mass(12), 2
-    assert_equal fuel_required.for_mass(14), 2
-    assert_equal fuel_required.for_mass(1969), 654
-    assert_equal fuel_required.for_mass(100756), 33583
+    assert_equal 2, fuel_required.for_mass(12)
+    assert_equal 2, fuel_required.for_mass(14)
+    assert_equal 654, fuel_required.for_mass(1969)
+    assert_equal 33583, fuel_required.for_mass(100756)
   end
 
   def test_calculates_total_fuel_needed
     fuel_required = FuelRequirement.new
 
-    assert_equal fuel_required.total([12, 14, 1969, 100756]), 102751
+    assert_equal 102751, fuel_required.total([12, 14, 1969, 100756])
   end
 
   def test_calculates_fuel_requirements_including_additional_fuel
     fuel_required = FuelRequirement.new
 
-    assert_equal fuel_required.for_mass_incl_fuel(14), 2
-    assert_equal fuel_required.for_mass_incl_fuel(1969), 966
-    assert_equal fuel_required.for_mass_incl_fuel(100756), 50346
+    assert_equal 2, fuel_required.for_mass_incl_fuel(14)
+    assert_equal 966, fuel_required.for_mass_incl_fuel(1969)
+    assert_equal 50346, fuel_required.for_mass_incl_fuel(100756)
   end
 end
