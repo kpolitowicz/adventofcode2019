c8ccbe9b2e0f9c3295366599d1e50c290fe2e0a3
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 09:49:56 -0500

    added script calculation the answer from inputs

---
+++ 01/part1_answer.rb
@@ -0,0 +1,14 @@
+require_relative 'fuel_requirement'
+
+input_file = "#{__dir__}/input.txt"
+
+fuel_required = FuelRequirement.new
+
+mass_of_modules = []
+File.foreach(input_file) do |line|
+  mass_of_modules << line.to_i
+end
+
+fuel_for_modules = mass_of_modules.map { |mass| fuel_required.for_mass(mass) }
+
+puts fuel_required.total(fuel_for_modules)
