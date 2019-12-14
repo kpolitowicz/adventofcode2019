7b28e72fed44723483c43605280af8fa7de5f0d9
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-14 13:27:32 -0500

    working run function!

--- 02/intcode_program.exs
+++ 02/intcode_program.exs
@@ -4,4 +4,36 @@ defmodule IntcodeProgram do
     |> Enum.map(&String.to_integer/1)
     |> List.to_tuple
   end
+
+  def run(program) do
+    execute_next_opcode(program, 0)
+  end
+
+  defp execute_next_opcode(program, current_position) do
+    opcode = elem(program, current_position)
+
+    case execute_opcode(opcode, program, current_position) do
+      {:ok, result} ->
+        res_pos = elem(program, current_position + 3)
+        program = put_elem(program, res_pos, result)
+        execute_next_opcode(program, current_position + 4)
+
+      :halt ->
+        program
+    end
+  end
+
+  defp execute_opcode(1, program, current_position) do
+    arg1_pos = elem(program, current_position + 1)
+    arg2_pos = elem(program, current_position + 2)
+
+    {:ok, elem(program, arg1_pos) + elem(program, arg2_pos)}
+  end
+  defp execute_opcode(2, program, current_position) do
+    arg1_pos = elem(program, current_position + 1)
+    arg2_pos = elem(program, current_position + 2)
+
+    {:ok, elem(program, arg1_pos) * elem(program, arg2_pos)}
+  end
+  defp execute_opcode(99, _program, _current_position), do: :halt
 end
--- 02/intcode_program_test.exs
+++ 02/intcode_program_test.exs
@@ -9,4 +9,11 @@ defmodule IntcodeProgramTest do
     string = "1,2,3"
     assert {1,2,3} = IntcodeProgram.read_from(string)
   end
+
+  test "runs the program" do
+    assert {2,0,0,0,99} = IntcodeProgram.run({1,0,0,0,99})
+    assert {2,3,0,6,99} = IntcodeProgram.run({2,3,0,3,99})
+    assert {2,4,4,5,99,9801} = IntcodeProgram.run({2,4,4,5,99,0})
+    assert {30,1,1,4,2,5,6,0,99} = IntcodeProgram.run({1,1,1,4,99,5,6,0,99})
+  end
 end
