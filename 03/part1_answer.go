30656e00d89bf39125ea11212e84cbee46ca0fe9
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-15 17:55:21 -0500

    commented out part1 answer

--- 03/part1_answer.go
+++ 03/part1_answer.go
@@ -1,14 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"strings"
-)
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"strings"
+// )
 
-func main() {
-	dat, _ := ioutil.ReadFile("input.txt")
-	ary := strings.Split(string(dat), "\n")
+// func main() {
+// 	dat, _ := ioutil.ReadFile("input.txt")
+// 	ary := strings.Split(string(dat), "\n")
 
-	fmt.Println(CalcManhattanDistance(ary[0], ary[1]))
-}
+// 	fmt.Println(CalcManhattanDistance(ary[0], ary[1]))
+// }
