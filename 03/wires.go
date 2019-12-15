920bb2e2fe0f5c6f346502d4c1a5ee706c65c1fc
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-15 16:00:43 -0500

    amost there

--- 03/wires.go
+++ 03/wires.go
@@ -11,6 +11,10 @@ type wireDef struct {
 	steps     int
 }
 
+type point struct {
+	x, y int
+}
+
 func ParseInputString(inputStr string) []string {
 	return strings.Split(inputStr, ",")
 }
@@ -22,10 +26,77 @@ func ConvertToWireDef(wireDirectives []string) (res []wireDef) {
 	return
 }
 
+func GetWirePoints(wireDefs []wireDef) (res []point) {
+	currentPos := point{0, 0}
+	for _, wireDef := range wireDefs {
+		res = append(res, pointsFrom(wireDef, currentPos)...)
+		currentPos = res[len(res)-1]
+	}
+	return
+}
+
+// TODO: implement wire args as set with intersection
+func FindCommonPoints(wire1, wire2 []point) (res []point) {
+	for _, wire1_point := range wire1 {
+		for _, wire2_point := range wire2 {
+			if wire1_point == wire2_point {
+				res = append(res, wire1_point)
+			}
+		}
+	}
+	return
+}
+
+func ManhattanDistance(p point) int {
+	return p.x + p.y
+}
+
 func parseWireDirectiveIntoDef(wireDir string) wireDef {
 	dir := wireDir[0]
 	steps, _ := strconv.Atoi(string(wireDir[1]))
 	return wireDef{dir, steps}
+}
+
+func pointsFrom(wireD wireDef, startPos point) (res []point) {
+	switch wireD.direction {
+	case 'R':
+		res = append(res, moveRight(wireD.steps, startPos)...)
+	case 'L':
+		res = append(res, moveLeft(wireD.steps, startPos)...)
+	case 'U':
+		res = append(res, moveUp(wireD.steps, startPos)...)
+	case 'D':
+		res = append(res, moveDown(wireD.steps, startPos)...)
+	}
+	return
+}
+
+func moveRight(steps int, startPos point) (res []point) {
+	for i := 1; i <= steps; i++ {
+		res = append(res, point{startPos.x + i, startPos.y})
+	}
+	return
+}
+
+func moveLeft(steps int, startPos point) (res []point) {
+	for i := 1; i <= steps; i++ {
+		res = append(res, point{startPos.x - i, startPos.y})
+	}
+	return
+}
+
+func moveUp(steps int, startPos point) (res []point) {
+	for i := 1; i <= steps; i++ {
+		res = append(res, point{startPos.x, startPos.y + i})
+	}
+	return
+}
+
+func moveDown(steps int, startPos point) (res []point) {
+	for i := 1; i <= steps; i++ {
+		res = append(res, point{startPos.x, startPos.y - i})
+	}
+	return
 }
 
 func main() {
--- 03/wires_test.go
+++ 03/wires_test.go
@@ -22,3 +22,53 @@ func TestConvertToWireDef(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestGetWirePoints(t *testing.T) {
+	got := GetWirePoints([]wireDef{wireDef{'R', 3}, wireDef{'U', 2}, wireDef{'L', 2}, wireDef{'D', 1}})
+	want := []point{
+		point{1, 0},
+		point{2, 0},
+		point{3, 0},
+		point{3, 1},
+		point{3, 2},
+		point{2, 2},
+		point{1, 2},
+		point{1, 1},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestFindCommonPoints(t *testing.T) {
+	wire1 := GetWirePoints([]wireDef{
+		wireDef{'R', 8},
+		wireDef{'U', 5},
+		wireDef{'L', 5},
+		wireDef{'D', 3},
+	})
+	wire2 := GetWirePoints([]wireDef{
+		wireDef{'U', 7},
+		wireDef{'R', 6},
+		wireDef{'D', 4},
+		wireDef{'L', 4},
+	})
+	got := FindCommonPoints(wire1, wire2)
+	want := []point{
+		point{6, 5},
+		point{3, 3},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestManhattanDistance(t *testing.T) {
+	got := ManhattanDistance(point{6, 5})
+	want := 11
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
