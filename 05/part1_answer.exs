6617cd4747867950a804dff348d10b9eee8725f7
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-16 21:17:09 -0500

    outputting the result of the program - correct!

--- 05/part1_answer.exs
+++ 05/part1_answer.exs
@@ -2,10 +2,8 @@ Code.require_file "intcode_program.exs", __DIR__
 
 {:ok, program_str} = File.read(__DIR__ <> "/input.txt")
 
-noun = 12
-verb = 2
-program =
+{_program, output} =
   IntcodeProgram.read_from(program_str)
-  |> IntcodeProgram.run_with_inputs(noun, verb)
+  |> IntcodeProgram.run([1])
 
-IO.puts IntcodeProgram.output(program)
+IO.puts hd(output)
